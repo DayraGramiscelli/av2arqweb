0afc0244b00931ab9716f3a4a609171bfef3e4b1
DayraGramiscelli <dayra@moonlightmobile.dev>
2025-06-27 20:03:12 -0300

    final final mesmo

--- autheticuser/src/main/java/com/example/autheticuser/controller/HomeController.java
+++ autheticuser/src/main/java/com/example/autheticuser/controller/HomeController.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-@Hidden // Oculta este controller da documentação do Swagger
+
 public class HomeController {
 
     @GetMapping("/")
